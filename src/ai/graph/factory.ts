75d7de91d47dc2f22e60374c39df214dc4335175
ThauanyAA <thauanyaraujo5@gmail.com>
2026-05-11 20:02:46 -0300

    Initial commit

---
+++ src/ai/graph/factory.ts
@@ -0,0 +1,13 @@
+import { buildGraphPipeline } from './graph';
+import { OpenRouterService } from '../services/openRouterService';
+
+export async function buildGraph() {
+  const llm = new OpenRouterService();
+  return buildGraphPipeline(llm);
+}
+
+export const graph = async () => {
+  return buildGraph();
+};
+
+export default graph;
---
+++ src/ai/services/mcpService.ts
@@ -0,0 +1,22 @@
+import { MultiServerMCPClient } from "@langchain/mcp-adapters";
+import { getMongodbTool } from "../tools/mongodbTool";
+import { getFileSystemTool } from "../tools/fileSystemTool";
+
+export const getMCPTools = async () => {
+  
+  const client = new MultiServerMCPClient({
+    mcpServers: {
+      ...getMongodbTool(),
+      ...getFileSystemTool()
+    },
+    onMessage: (log, source) => {
+      console.log(`[${source.server}]`, log.data);
+    }
+  })
+
+  const mcpTools = await client.getTools();
+
+  return [
+    ...mcpTools
+  ];
+};
